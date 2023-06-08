91a0b2559e6c208ef9e4cefc3e2c856d308e9982
bouzrbay <atighmarcelline@gmail.com>
2023-06-08 10:03:00 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
 
-from add_0 import add
-
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
