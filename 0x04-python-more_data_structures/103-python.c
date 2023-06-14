456e290ab6c453e3bfd48a9c249781200ea81fd8
bouzrbay <atighmarcelline@gmail.com>
2023-06-14 20:57:19 +0100

    correction

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -18,7 +18,8 @@ void print_python_list(PyObject *p)
     printf("[*] Size of the Python List = %ld\n", size);
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
+    {
         PyObject *item = PyList_GetItem(p, i);
         const char *itemType = Py_TYPE(item)->tp_name;
 
@@ -42,7 +43,8 @@ void print_python_bytes(PyObject *p)
     Py_ssize_t i;
 
     printf("[.] Bytes object info\n");
-    if (!PyBytes_Check(p)) {
+    if (!PyBytes_Check(p))
+    {
         printf("  [ERROR] Invalid Bytes Object\n");
         return;
     }
@@ -54,7 +56,8 @@ void print_python_bytes(PyObject *p)
     printf("  trying string: %s\n", str);
 
     printf("  first %ld bytes: ", size + 1 > 10 ? 10 : size + 1);
-    for (i = 0; i < size + 1 && i < 10; i++) {
+    for (i = 0; i < size + 1 && i < 10; i++)
+    {
         printf("%02hhx", str[i]);
         if (i + 1 < size + 1 && i + 1 < 10)
             printf(" ");
