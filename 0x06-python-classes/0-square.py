84442365bb79de330bbdfe05011754b8ba5de32f
bouzrbay <atighmarcelline@gmail.com>
2023-06-27 19:32:51 +0100

    first_task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+class Square:
+    def __init__(self, side_length):
+        self.side_length = side_length
+    
+    def get_area(self):
+        return self.side_length ** 2
+    
+    def get_perimeter(self):
+        return 4 * self.side_length
