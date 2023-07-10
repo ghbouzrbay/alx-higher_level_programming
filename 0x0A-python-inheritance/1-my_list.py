9f3f58638ab55d4ace97d4ad56c277bdae62a3a5
bouzrbay <atighmarcelline@gmail.com>
2023-07-10 21:13:12 +0100

    the mondatory tasks of Python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 
 """Defines an inherited list class MyList."""
 
- class MyList(list):
+class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
