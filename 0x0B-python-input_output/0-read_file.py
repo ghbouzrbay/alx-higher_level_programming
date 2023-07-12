345e1ac8f963ee8dbba6769a6b787dfe17e37356
bouzrbay <atighmarcelline@gmail.com>
2023-07-12 19:29:05 +0100

    correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,6 @@ def read_file(filename=""):
        Args:
            filename (str): content of the file. Defaults to
     """
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
+
