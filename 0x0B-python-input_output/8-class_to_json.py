9c4506da2b2ec19db3b64a6e27278da57375b4ec
bouzrbay <atighmarcelline@gmail.com>
2023-07-12 20:35:51 +0100

    correction

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+"""Defines a Python class-to-JSON function."""
 
-"""function that returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean) for JSON serialization of an object"""
 
 def class_to_json(obj):
-    """class_to_json return dictionary description with simple data structure
-
-    Args:
-        obj : any object for example list, dict
-    """
+    """Return the dictionary represntation of a simple data structure."""
     return obj.__dict__
