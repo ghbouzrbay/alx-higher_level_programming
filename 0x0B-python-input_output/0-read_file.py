2c7f842e2941de613dfe736e15320a186e3d0b25
bouzrbay <atighmarcelline@gmail.com>
2023-07-12 20:52:45 +0100

    correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-
 """Defines a text file-reading function."""
 
+
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout.
-       Args:
-           filename (str): content of the file. Defaults to
-    """
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-
