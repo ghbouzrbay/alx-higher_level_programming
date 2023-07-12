1966bc7a4567f9bc361ff87bc19b501b098d9812
bouzrbay <atighmarcelline@gmail.com>
2023-07-12 20:36:52 +0100

    correction

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
+"""Defines a class Student."""
 
-"""class Student that defines a student """
 
 class Student:
     """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """__init__ initialized constructor
+        """Initialize a new Student.
 
         Args:
-            first_name (str): first name
-            last_name (str: last name
-            age (int): age
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """retrieves a dictionary represt """
+        """Get a dictionary representation of the Student."""
         return self.__dict__
