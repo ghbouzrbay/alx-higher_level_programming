bf000922a8f7cfdab14bd84a83833262f021d28f
ghbouzrbay <atighmarcelline@gmail.com>
2023-09-12 19:26:45 +0000

    Done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,10 @@
-!/usr/bin/node
-const square = require('./5-square');
-class Square extends square {
-  /**
-   * @property {method} charPrint - prints the rectangle using the character c
-   * @returns void
-   */
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
-}
-module.exports = Square;
+};
