81eba429c159b2c1106edf8e9163066c208b24c1
ghbouzrbay <atighbanuculla@gmail.com>
2023-09-29 12:00:56 -0700

    correction_style

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Module that finds a peak in a list of integers"""
-
 def find_peak(list_of_integers):
     """function that finds a peak in a list of unsorted integers.
-
     Args:
         list_of_integers (list): a list of integers
 
@@ -18,7 +16,8 @@ def find_peak(list_of_integers):
     start, end = 0, length - 1
     while start < end:
         middel = start + (end - start) // 2
-        if int_list[middel] > int_list[middel - 1] and int_list[middel] > int_list[middel + 1]:
+        if int_list[middel] > int_list[middel - 1] and
+        int_list[middel] > int_list[middel + 1]:
             return int_list[middel]
         if int_list[middel - 1] > int_list[middel + 1]:
             end = middel
