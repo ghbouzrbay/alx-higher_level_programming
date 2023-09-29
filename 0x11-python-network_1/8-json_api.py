a929435552f04fe3432cca1f03c6d741b1e3b81e
ghbouzrbay <atighbanuculla@gmail.com>
2023-09-29 11:57:39 -0700

    Mondatory

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,11 +4,9 @@
 import sys
 import requests
 
-
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
-
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
         response = r.json()
