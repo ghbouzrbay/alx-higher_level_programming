b6fdeb05987f796351fb6d1fd6278619b026b4bd
ghbouzrbay <atighbanuculla@gmail.com>
2023-09-29 12:03:27 -0700

    style

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """script that takes in a URL, sends a request to the URL and displays the body of the response"""
-
 import sys
 import requests
 
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    response = requests.post("http://0.0.0.0:5000/search_user",
+                             data=payload)
     try:
         response = r.json()
         if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(response.get("id"),
+                                   response.get("name")))
     except ValueError:
         print("Not a valid JSON")
