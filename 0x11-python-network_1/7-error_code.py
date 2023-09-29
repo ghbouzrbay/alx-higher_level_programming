570b39d27041d893140070745d55d79d38482f79
bouzrbay <atighmarcelline@gmail.com>
2023-09-29 18:20:26 +0100

    add in code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,4 +12,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
