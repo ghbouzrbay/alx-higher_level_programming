cde825117738a6ec15f54ee5b093c7754a5a3fb5
ghbouzrbay <atighbanuculla@gmail.com>
2023-10-24 09:28:58 -0700

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
-   console.log(err);
+    console.log(err);
   } else {
     process.stdout.write(data);
   }
