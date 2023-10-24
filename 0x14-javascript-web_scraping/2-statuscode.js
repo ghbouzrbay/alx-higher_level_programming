205661d2f9cbd64a458cabd6286bac9211812cc0
ghbouzrbay <atighbanuculla@gmail.com>
2023-10-24 09:33:05 -0700

    Done

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response) {
+  if (error == null) {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
